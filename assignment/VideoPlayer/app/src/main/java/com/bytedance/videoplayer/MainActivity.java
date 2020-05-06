4828c8dc680f438ceeb1aef17e9be75ea903a0b3
TJenica <158155978@qq.com>
2020-05-06 16:51:27 +0800

    first commit

---
+++ assignment/VideoPlayer/app/src/main/java/com/bytedance/videoplayer/MainActivity.java
@@ -0,0 +1,55 @@
+package com.bytedance.videoplayer;
+
+import android.content.res.Configuration;
+
+import android.net.Uri;
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.widget.ImageView;
+
+import android.widget.MediaController;
+
+import android.widget.VideoView;
+
+
+
+import com.bumptech.glide.Glide;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private CustomizeVideoview videoView;
+    private MediaController mediaController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        videoView = findViewById(R.id.video_view);
+        mediaController = new MediaController(this);
+        mediaController.setMediaPlayer(videoView);
+        videoView.setMediaController(mediaController);
+
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/raw/" + R.raw.workaholic);
+        videoView.setVideoURI(uri);
+        videoView.start();
+
+    }
+
+
+
+    @Override
+
+    public void onConfigurationChanged(Configuration newConfig) {
+
+        super.onConfigurationChanged(newConfig);
+
+    }
+
+}
