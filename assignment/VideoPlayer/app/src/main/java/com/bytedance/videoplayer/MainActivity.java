407c1d1c1edbd298b266a8ca774a2e98b04ef20d
TJenica <158155978@qq.com>
2020-05-08 11:18:21 +0800

    firat commit

--- assignment/VideoPlayer/app/src/main/java/com/bytedance/videoplayer/MainActivity.java
+++ assignment/VideoPlayer/app/src/main/java/com/bytedance/videoplayer/MainActivity.java
@@ -1,23 +1,11 @@
 package com.bytedance.videoplayer;
 
+import android.content.Intent;
 import android.content.res.Configuration;
-
 import android.net.Uri;
-
 import android.os.Bundle;
-
 import android.support.v7.app.AppCompatActivity;
-
-import android.widget.ImageView;
-
 import android.widget.MediaController;
-
-import android.widget.VideoView;
-
-
-
-import com.bumptech.glide.Glide;
-
 
 
 public class MainActivity extends AppCompatActivity {
@@ -36,20 +24,26 @@ public class MainActivity extends AppCompatActivity {
         mediaController.setMediaPlayer(videoView);
         videoView.setMediaController(mediaController);
 
-        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/raw/" + R.raw.workaholic);
-        videoView.setVideoURI(uri);
+//        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/raw/" + R.raw.workaholic);
+//        videoView.setVideoURI(uri);
+        Intent intent = getIntent();
+        Uri realUri;
+        Uri data = intent.getData();
+        if(data != null) {
+            realUri = data;
+        } else {
+            realUri = Uri.parse("android.resource://" + getPackageName() + "/raw/" + R.raw.workaholic);
+        }
+
+        videoView.setVideoURI(realUri);
         videoView.start();
 
     }
 
-
-
     @Override
 
     public void onConfigurationChanged(Configuration newConfig) {
-
         super.onConfigurationChanged(newConfig);
-
     }
 
 }
